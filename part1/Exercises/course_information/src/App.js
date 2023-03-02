22588f7b83fc57ab8323c3aa77062712bfe9f56b
sami <testi@git.com>
2023-03-02 13:04:53 +0200

    changed the location of components due to earlier misunderstanding

--- part1/Exercises/course_information/src/App.js
+++ part1/Exercises/course_information/src/App.js
@@ -1,3 +1,37 @@
+const Header = props => {
+  return (
+    <h1>{props.course}</h1>
+  );
+}
+
+const Part = props => {
+  return (
+    <p>{props.part} {props.exercises}</p>
+  );
+}
+
+const Content = (props) => {
+  const part1 = `${props.parts[0].name} ${props.parts[0].exercises}`;
+  const part2 = `${props.parts[1].name} ${props.parts[1].exercises}`;
+  const part3 = `${props.parts[2].name} ${props.parts[2].exercises}`;
+
+  return (
+    <div>
+      <Part part={part1}/>
+      <Part part={part2}/>
+      <Part part={part3}/>
+    </div>
+  );
+}
+
+const Total = props => {
+  let total = null;
+  props.parts.map(part => total += part.exercises)
+  return (
+    <p>Number of exercises {total}</p>
+  );
+}
+
 const App = () => {
   const course = {
     name: 'Half Stack application development',
@@ -15,40 +49,6 @@ const App = () => {
         exercises: 14
       }
     ]
-  }
-
-  const Header = props => {
-    return (
-      <h1>{props.course}</h1>
-    );
-  }
-
-  const Part = props => {
-    return (
-      <p>{props.part} {props.exercises}</p>
-    );
-  }
-
-  const Content = (props) => {
-    const part1 = `${props.parts[0].name} ${props.parts[0].exercises}`;
-    const part2 = `${props.parts[1].name} ${props.parts[1].exercises}`;
-    const part3 = `${props.parts[2].name} ${props.parts[2].exercises}`;
-
-    return (
-      <div>
-        <Part part={part1}/>
-        <Part part={part2}/>
-        <Part part={part3}/>
-      </div>
-    );
-  }
-
-  const Total = props => {
-    let total = null;
-    props.parts.map(part => total += part.exercises)
-    return (
-      <p>Number of exercises {total}</p>
-    );
   }
 
   return (
